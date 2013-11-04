b241b590d60a58085f4d14795521db97d59f0a36
Ralf Westphal <info@ralfw.de>
2013-11-04 15:02:24 +0100

    Initial
    Kontrakt angelegt

---
+++ source/af.contracts/af.contracts/Befragung.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace af.contracts
+{
+    public class Befragung
+    {
+        public List<Frage> Fragen;
+
+        public class Frage
+        {
+            public string Text;
+            public List<Antwortmöglichkeit> Antwortmöglichkeiten;
+        }
+
+        public class Antwortmöglichkeit
+        {
+            public string Id;
+            public string Text;
+            public bool IstRichtigeAntwort;
+            public bool IstAlsAntwortSelektiert;
+        }
+
+
+        public void Reset()
+        {
+            Fragen = new List<Frage>();
+        }
+    }
+}
---
+++ source/af.contracts/af.contracts/IComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace af.contracts
+{
+    public interface IComponent
+    {
+        void Process(string json);
+        event Action<string> Json_output;
+    }
+}
