05db3f86d93937e361c2c3a2ab668f421b8221e3
PaS76 <patrick.suwito@ma-design.de>
2013-11-15 13:54:04 +0100

    modified befragen module

---
+++ source/af.modul.befragen/af.modul.befragen.test/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using af.contracts;
+using jsonserialization;
+
+namespace af.modul.befragen.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string filename = null;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var befragung = new Befragung();
+            befragung.Reset();
+            befragung.Fragen.Add(
+                new Befragung.Frage
+                    {
+                        Text = "Was ist kein Säugetier?",
+                        Antwortmöglichkeiten = new List<Befragung.Antwortmöglichkeit>
+                                                   {
+                                                       new Befragung.Antwortmöglichkeit
+                                                           {
+                                                               Id = "F1A1",
+                                                               Text = "Hund"
+                                                           },
+                                                       new Befragung.Antwortmöglichkeit
+                                                           {
+                                                               Id = "F1A2",
+                                                               Text = "Katze"
+                                                           },
+                                                       new Befragung.Antwortmöglichkeit
+                                                           {
+                                                               Id = "F1A3",
+                                                               Text = "Fisch",
+                                                               IstRichtigeAntwort = true
+                                                           },
+                                                       new Befragung.Antwortmöglichkeit
+                                                           {
+                                                               Id = "F1A4",
+                                                               Text = "Weiß nicht"
+                                                           },
+                                                   }
+                    });
+            var befragen = new Befragen(befragung);
+            filename = "FragekatalogTestName1";
+            befragen.Process(filename);
+            befragen.Json_output += OnResponse;
+        }
+
+        private void OnResponse(string obj)
+        {
+            dynamic output = obj.FromJson();
+            Assert.AreEqual(output.Fragen.Text, filename);
+        }
+    }
+}
--- source/af.modul.befragen/af.modul.befragen/Befragen.cs
+++ source/af.modul.befragen/af.modul.befragen/Befragen.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 
 using af.contracts;
+
+using jsonserialization;
 
 namespace af.modul.befragen
 {
@@ -15,7 +20,22 @@ namespace af.modul.befragen
 
         public void Process(string json)
         {
-            throw new NotImplementedException();
+            dynamic input = json.FromJson();
+            switch ((string)input.cmd)
+            {
+                case "Fragenkatalog laden":
+                    var dateiname = (string)input.payload.Dateiname;
+                    // Öffne Datei TODO: öffne Datei
+                    // Lese zeilenweise
+                    // Liste von Fragen und Antwortmöglichkeiten in einen Fragebogen wandeln.
+                    // Resette Befragung
+                    _befragung.Reset(); // Erzeugt neue Antwortliste!   
+                    // Erzeuge Fragebogen aus Fragekatalog, als Text der ersten Frage für den Durchstich nur der Dateiname zurück
+                    _befragung.Fragen.Add(new Befragung.Frage { Text = dateiname });
+                    // Schicke Fragebogen
+                    Json_output(_befragung.ToJson());
+                    break;
+            }
         }
 
         public event Action<string> Json_output;
