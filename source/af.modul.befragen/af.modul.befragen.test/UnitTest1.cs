fbb0ecbd71fcec217b9462b91dea75e87228a616
PaS76 <patrick.suwito@ma-design.de>
2013-11-22 09:53:08 +0100

    [Befragen]: Added first snapshot of fixed test, again!

--- source/af.modul.befragen/af.modul.befragen.test/UnitTest1.cs
+++ source/af.modul.befragen/af.modul.befragen.test/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Dynamic;
+﻿using System.Dynamic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using af.contracts;
@@ -10,52 +9,28 @@ namespace af.modul.befragen.test
     [TestClass]
     public class UnitTest1
     {
-        private string filename = null;
+        private const string FILENAME = "Test-dateiname";
 
         [TestMethod]
         public void TestMethod1()
         {
-            var befragung = new Befragung();
-            befragung.Reset();
-            befragung.Fragen.Add(
-                new Befragung.Frage
-                    {
-                        Text = "Was ist kein Säugetier?",
-                        Antwortmöglichkeiten = new List<Befragung.Antwortmöglichkeit>
-                                                   {
-                                                       new Befragung.Antwortmöglichkeit
-                                                           {
-                                                               Id = "F1A1",
-                                                               Text = "Hund"
-                                                           },
-                                                       new Befragung.Antwortmöglichkeit
-                                                           {
-                                                               Id = "F1A2",
-                                                               Text = "Katze"
-                                                           },
-                                                       new Befragung.Antwortmöglichkeit
-                                                           {
-                                                               Id = "F1A3",
-                                                               Text = "Fisch",
-                                                               IstRichtigeAntwort = true
-                                                           },
-                                                       new Befragung.Antwortmöglichkeit
-                                                           {
-                                                               Id = "F1A4",
-                                                               Text = "Weiß nicht"
-                                                           },
-                                                   }
-                    });
-            var befragen = new Befragen(befragung);
-            filename = "FragekatalogTestName1";
-            befragen.Process(filename);
-            befragen.Json_output += OnResponse;
-        }
+            dynamic input = new ExpandoObject();
+            input.cmd = "Fragenkatalog laden";
+            input.payload = new ExpandoObject();
 
-        private void OnResponse(string obj)
-        {
-            dynamic output = obj.FromJson();
-            Assert.AreEqual(output.Fragen.Text, filename);
+            input.payload.Dateiname = FILENAME;
+
+            var befragen = new Befragen(new Befragung{ Dateiname = FILENAME});
+            var jsonResult = string.Empty;
+            befragen.Json_output += _ => jsonResult = _;
+
+            var json = JsonExtensions.ToJson(input);
+            befragen.Process(json);
+
+            dynamic result = jsonResult.FromJson();
+
+            Assert.AreEqual(result.Dateiname, FILENAME);
+
         }
     }
 }
