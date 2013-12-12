c1e38a2559fde29a09249ff24096fa4d6a649c10
Ralf Westphal <info@ralfw.de>
2013-12-12 11:55:33 +0100

    Build script für jeden Rechner
    muss MsBuild im Pfad haben
    af.app baut nach bin

--- build/buildOnAppVeyor.bat
+++ build/buildOnAppVeyor.bat
@@ -3,8 +3,10 @@ msbuild.exe %srcFolder%\source\jsonserialization\jsonserialization.sln
 msbuild.exe %srcFolder%\source\af.contracts\af.contracts.sln
 
 msbuild.exe %srcFolder%\source\af.modul.auswerten\af.modul.auswerten.sln
-msbuild.exe %srcFolder%\source\af.modul.befragen\af.module.befragen.sln
+msbuild.exe %srcFolder%\source\af.modul.befragen\af.modul.befragen.sln
 msbuild.exe %srcFolder%\source\af.ui\af.ui.sln
+msbuild.exe %srcFolder%\source\af.application\af.application.2010.sln
+rem msbuild.exe %srcFolder%\source\af.application\af.application.sln
 
 msbuild.exe %srcFolder%\source\demo\demo.sln
 
