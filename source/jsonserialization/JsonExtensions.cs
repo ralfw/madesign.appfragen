780e825ae157f4f3da1f4d662e373b41e590bc2a
PaS76 <patrick.suwito@ma-design.de>
2013-12-12 10:15:57 +0100

    [jsonextensions]: Removed unneeded reference to System.Web.Helpers

--- source/jsonserialization/JsonExtensions.cs
+++ source/jsonserialization/JsonExtensions.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web.Helpers;
-using ImpromptuInterface;
 
 namespace jsonserialization
 {
